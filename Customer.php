782225b2de879eec5decd257c644418da2f8ee50
MSNatama <101536511+MSNatama@users.noreply.github.com>
2022-06-06 22:32:29 +0800

    Update Customer.php

--- Customer.php
+++ Customer.php
@@ -1,21 +1,33 @@
 <?php 
+    ob_start();
+    require('components/header.inc.php');
     require('Model.php');
     if (isset($_GET['id_customer'])) {
         DeleteCake($_GET['id_customer']);
     }
 ?>
-<div class="col-md-12">
-    <h1>Customer Data</h1>
-    <table class="rwd-table">
-            <tr>
-                <th >Customer Name </th>
-                <th >Number </th>
-                <th >Address </th>
-                <th >Registered Date </th>
-                <th >Action</th>
-            </tr>
-            <?php
-                ShowCustomer("pelanggan")
-            ?>
-    </table>
+<div class="album py-5 bg-light">
+    <section class="py-4 text-center container">
+        <div class="col-lg-6 col-md-8 mx-auto">
+            <h1>Customer Data</h1>
+            <p>
+                <a href="FormCustomer.php" class="btn btn-primary my-2">Add Customer Data</a>
+            </p>
+        </div>
+    </section>
+    <div class="col-md-12">
+        <table class="rwd-table">
+                <tr>
+                    <th >Customer Name </th>
+                    <th >Number </th>
+                    <th >Address </th>
+                    <th >Registered Date </th>
+                    <th >Action</th>
+                </tr>
+                <?php
+                    ShowCustomer("pelanggan")
+                ?>
+        </table>
+    </div>
 </div>
+<?php require('components/footer.inc.php')?>
