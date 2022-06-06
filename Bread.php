45825b553d9532429abb3c28674ff9496975cefa
mfazapl <101613106+mfazapl@users.noreply.github.com>
2022-06-06 21:57:40 +0700

    Add files via upload

---
+++ Bread.php
@@ -0,0 +1,21 @@
+<?php 
+    require('Model.php');
+    if (isset($_GET['id'])) {
+        DeleteCake($_GET['id']);
+    }
+?>
+<link rel="stylesheet" href="assets/dist/css/style.css">
+<div class="col-md-12">
+    <h1>Bread Table</h1>
+    <table class="rwd-table">
+            <tr>
+                <th >Bread Name </th>
+                <th >Bread Type </th>
+                <th >Price </th>
+                <th >Action</th>
+            </tr>
+            <?php
+                ShowBread("roti")
+            ?>
+    </table>
+</div>
---
+++ BreadPage.php
@@ -0,0 +1,6 @@
+<?php require('components/header.inc.php')?>
+<?php require('components/custom_section.php')?>
+<div class="album py-5 bg-light">
+  <?php require('Bread.php'); ?>
+</div>
+<?php require('components/footer.inc.php')?>
