0872d02adbed9d7f76307ed7d3dbec4511ec1898
MSNatama <101536511+MSNatama@users.noreply.github.com>
2022-06-07 01:14:39 +0800

    Update home.inc.php

--- components/home.inc.php
+++ components/home.inc.php
@@ -1,15 +1,15 @@
-<section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">About Us</h1>
-        <p class="lead text-muted">bla bla bla</p>
-      </div>
-    </div>
-</section>
 <div class="album py-5 bg-light">
+<section class="py-4 text-center container">
+        <div class="col-lg-6 col-md-8 mx-auto">
+            <h1>Order Data</h1>
+            <p>
+                <a href="FormCakeOrder.php" class="btn btn-primary my-2">Add Order</a>
+            </p>
+        </div>
+    </section>
     <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <?php require('components/collection.php'); ?>
+        <?php require('COrder.php'); ?>
       </div>
     </div>
 </div>
